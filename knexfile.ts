f0843705309d7340d448d81b3b244f6fd2ec4309
CharlesChanLok <charleschanlok@gmail.com>
2018-05-19 23:39:27 +0800

    changed database url in knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -6,12 +6,8 @@ module.exports = {
 
   development: {
     client: 'postgresql',
-    connection: {
-      host: process.env.DATABASE_URL,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD
-    },
+    connection:  process.env.DATABASE_URL
+    ,
     pool: {
       min: 2,
       max: 10
