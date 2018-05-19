42b9bca6ee5923be140a988bfb4a684d9e90c943
CharlesChanLok <charleschanlok@gmail.com>
2018-05-19 23:58:18 +0800

    knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -6,8 +6,8 @@ module.exports = {
 
   development: {
     client: 'postgresql',
-    connection:  process.env.DATABASE_URL
-    ,
+    connection:  process.env.DATABASE_URL,
+    ssl: true,
     pool: {
       min: 2,
       max: 10
@@ -36,8 +36,8 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection:  process.env.DATABASE_URL
-    ,
+    connection:  process.env.DATABASE_URL,
+    ssl: true,
     pool: {
       min: 2,
       max: 10
