36d2eb44885ad2380d24cf5763500b15aabfeafc
Charles Chan <charleschanlok@gmail.com>
2019-01-19 23:37:12 +0800

    added pet and age column to users table

--- postgres/tables/users.sql
+++ postgres/tables/users.sql
@@ -5,6 +5,8 @@ CREATE TABLE users (
     name VARCHAR(100),
     email text UNIQUE NOT NULL,
     entries BIGINT DEFAULT 0,
+    age SMALLINT NOT NULL DEFAULT 0,
+    pet VARCHAR(100) NOT NULL DEFAULT '',
     joined TIMESTAMP DEFAULT now()
 );
 
